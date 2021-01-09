be05f8ee4ae4d89a0b23234d9a101f96d4c432ae
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-09 10:37:52 -0300

    add test utility to capture errors easier in our tests

--- simple.js
+++ simple.js
@@ -1,15 +1,36 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
-let result = sum(3, 7);
-let expected = 10;
+test("sum adds numbers", () => {
+  let result = sum(3, 7);
+  let expected = 10;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
 
-result = subtract(7, 3);
-expected = -4;
+test("subtract subtracts numbers", () => {
+  let result = subtract(7, 3);
+  let expected = -4;
 
-expect(result).toBe(expected);
+  expect(result).toBe(expected);
+});
+
+test("subtract subtracts numbers", () => {
+  let result = subtract(7, 3);
+  let expected = 4;
+
+  expect(result).toBe(expected);
+});
+
+function test(title, callback) {
+  try {
+    callback();
+    console.log(`[ok] ${title}`);
+  } catch (error) {
+    console.error(`[x] ${title}`);
+    console.error(error);
+  }
+}
 
 function expect(actual) {
   return {
