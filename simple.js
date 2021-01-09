ce3293352e2a093be09dead7aeff6c990fb4edf1
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-09 10:30:18 -0300

    simple abstraction to test code

--- simple.js
+++ simple.js
@@ -4,13 +4,20 @@ const subtract = (a, b) => a - b;
 let result = sum(3, 7);
 let expected = 10;
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
-}
+expect(result).toBe(expected);
 
 result = subtract(7, 3);
 expected = -4;
 
-if (result !== expected) {
-  throw new Error(`${result} is not equal to ${expected}`);
+expect(result).toBe(expected);
+
+function expect(actual) {
+  return {
+    toBe(expected) {
+      if (actual !== expected) {
+        throw new Error(`${actual} is not equal to ${expected}`);
+      }
+    },
+    toEqual(expected) {},
+  };
 }
