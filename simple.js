564ab4eecf4060693620795bd42f593b66c4182d
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-09 10:51:37 -0300

    use globals

--- simple.js
+++ simple.js
@@ -38,24 +38,3 @@ test("sumAsync adds numbers asynchronously", async () => {
 
   expect(result).toBe(10);
 });
-
-async function test(title, callback) {
-  try {
-    await callback();
-    console.log(`[ok] ${title}`);
-  } catch (error) {
-    console.error(`[x] ${title}`);
-    console.error(error);
-  }
-}
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`);
-      }
-    },
-    toEqual(expected) {},
-  };
-}
