2cb275f7d3ac60f9fc819af81650b7fae9645ff3
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-09 12:07:11 -0300

    move functions to globals

--- globals.js
+++ globals.js
@@ -19,5 +19,25 @@ function expect(actual) {
   };
 }
 
+function fn(impl) {
+  const mockFn = (...args) => {
+    mockFn.mock.calls.push(args);
+    return impl(...args);
+  };
+
+  mockFn.mock = { calls: [] };
+  mockFn.mockImplementation = (newImpl) => (impl = newImpl);
+
+  return mockFn;
+}
+
+function spyOn(obj, prop) {
+  const originalValue = obj[prop];
+  obj[prop] = fn();
+  obj[prop].mockRestore = () => (obj[prop] = originalValue);
+}
+
 global.test = test;
 global.expect = expect;
+global.fn = fn;
+global.spyOn = spyOn;
--- thumbWar.test.js
+++ thumbWar.test.js
@@ -8,24 +8,6 @@ require.cache[utilsPath] = {
   },
 };
 
-function fn(impl) {
-  const mockFn = (...args) => {
-    mockFn.mock.calls.push(args);
-    return impl(...args);
-  };
-
-  mockFn.mock = { calls: [] };
-  mockFn.mockImplementation = (newImpl) => (impl = newImpl);
-
-  return mockFn;
-}
-
-function spyOn(obj, prop) {
-  const originalValue = obj[prop];
-  obj[prop] = fn();
-  obj[prop].mockRestore = () => (obj[prop] = originalValue);
-}
-
 const assert = require("assert");
 const thumbWar = require("./thumbWar");
 const utils = require("./utils");
