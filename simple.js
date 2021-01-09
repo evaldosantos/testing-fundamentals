85a9a73fdcad9c82e4b374fa2af0ad421b84f26e
Evaldo Santos <jesantosjr@protonmail.com>
2021-01-09 10:42:53 -0300

    handling async code

--- simple.js
+++ simple.js
@@ -1,6 +1,9 @@
 const sum = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
+const sumAsync = (a, b) => Promise.resolve(a + b);
+const subtractionAsync = (a, b) => Promise.resolve(a - b);
+
 test("sum adds numbers", () => {
   let result = sum(3, 7);
   let expected = 10;
@@ -22,9 +25,23 @@ test("subtract subtracts numbers", () => {
   expect(result).toBe(expected);
 });
 
-function test(title, callback) {
+test("subtractAsync subtracts numbers asynchronously", async () => {
+  const result = await subtractionAsync(7, 3);
+  const expected = -4;
+
+  expect(result).toBe(10);
+});
+
+test("sumAsync adds numbers asynchronously", async () => {
+  const result = await sumAsync(7, 3);
+  const expected = 10;
+
+  expect(result).toBe(10);
+});
+
+async function test(title, callback) {
   try {
-    callback();
+    await callback();
     console.log(`[ok] ${title}`);
   } catch (error) {
     console.error(`[x] ${title}`);
